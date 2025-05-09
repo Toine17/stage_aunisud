5d9d9d5a78200d2442623aab3b30aeb18b12979b
Toine17 <Bersegeayantoine@yahoo.fr>
2025-05-09 10:20:18 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ menu = st.sidebar.radio(
 
 if menu == "Accueil":
     st.subheader("🔴⚫Bienvenue sur la page du stage Aunisud triathlon 2025 ")
-    st.write("Nous serons 16 à participer au stage cette année.")
-    st.write("Direction le Pays Basques espagnol du 29 mai au 1er juin.")
+    st.write("Nous serons 16 participants au stage cette année")
+    st.write("Direction le Pays Basque espagnol du 29 mai au 1er juin")
+    st.write("Le menu est à ouvrir sur la gauche de l'écran")
     st.image("groupe_accueil.jpg")
 
 # PROGRAMME #############################
@@ -23,10 +24,10 @@ elif menu == "Programme":
     st.subheader("Déroulé du stage")
     jour = st.selectbox("Choix de la journée" , ["Jour 1","Jour 2","Jour 3","Jour 4"])
     if jour == "Jour 1":
-        st.write("🚙 Départ de la piscine de Surgères avec 2 minibus, heure à définir")
-        st.write("Trajet jusqu'en début d'après-midi, prévoir un pique-nique")
-        st.write("Arrivée au gîte, 🚴‍♂️ première sortie vélo 48km 1000m D+")
-        st.write("Prévoir la tenue club pour faire la photo du stage")
+        st.write("🚙 Départ de la piscine de Surgères avec 2 minibus, heure à définir ⏱️")
+        st.write("Trajet jusqu'en début d'après-midi, prévoir un pique-nique 🥪")
+        st.write("Arrivée au gîte, 🚴‍♂️ première sortie vélo de 48 km avec 1000 m D+")
+        st.write("👕 Prévoir la tenue du club pour la photo de groupe du stage")
         st.write("Parcours")
         st.image("map_jour_1.jpg")
         st.write("Profil")
@@ -34,7 +35,7 @@ elif menu == "Programme":
         dropbox_link_1 = "https://www.dropbox.com/scl/fi/4f85nhlffifvurx1i3ntn/Jour-1.gpx?rlkey=ds6h3gw7yy8ex0g7jwnu1j3aa&st=nhe3wcqc&dl=1"
         st.markdown(f"[📥 Télécharger le GPX]({dropbox_link_1})", unsafe_allow_html=True)
     if jour == "Jour 2":
-        st.write("🚴‍♂️ Sortie longue à vélo, 2 groupes por 2 distances avec départ en commun")
+        st.write("🚴‍♂️ Sortie longue à vélo, 2 groupes pour 2 distances avec départ en commun")
         st.write("Les 45 premiers kilomètres sont communs aux 2 groupes")
         col1, col2 = st.columns(2)
 
@@ -59,8 +60,8 @@ elif menu == "Programme":
             st.markdown(f"[📥 Télécharger le GPX]({dropbox_link_2_longue})", unsafe_allow_html=True)
 
     if jour == "Jour 3":
-            st.write("En autonomie, plusieurs parcours sont disponibles dans la section Parcours.")
-            st.write("Natation en mer en fin de journée")
+            st.write("En autonomie, plusieurs parcours sont disponibles dans la section Parcours 🛣️")
+            st.write("Natation en mer en fin de journée à la plage de Zarautz🏊🏖️")
 
     if jour == "Jour 4":
             st.write("Courte activité le matin en autonomie")
@@ -71,14 +72,22 @@ elif menu == "Programme":
 elif menu == "Infos pratiques":
     st.subheader("🏨 Logement")
     st.write("Il se peut que nous ne soyons pas seuls dans le logement.")
-    lien_logement = "https://www.booking.com/hotel/es/saskarate.fr.html?aid=304142&label=gen173nr-1FCCsoggI46AdIDVgEaE2IAQGYAQ24ARfIAQzYAQHoAQH4AQaIAgGoAgO4AtbG88AGwAIB0gIkZTRmOWNkOTAtYjBmMC00ODc5LWIzODAtNzA5NDFjZGFmZjIz2AIF4AIB"
-    st.write("4 chambres triples, 1 chambre quadruple")
+    lien_logement = "https://www.gites.fr/gites_saskarate_asteasu_h5496447.htm"
+    st.write("4 chambres triples, 1 chambre quadruple 🛏️")
     st.write("Selon l'agencement des chambres on essaiera de déplacer un lit pour faire une chambre double pour les filles et une deuxième chambre quadruple.")
     st.markdown(f"[Ici les informations sur le logement]({lien_logement})", unsafe_allow_html=True)
+    
     st.subheader("🏊 Natation")
     st.write("La plage de Zarautz est à 30 minutes en voiture et la piscine municipale d'Aya à 17 minutes.")
+    
     st.subheader(" 🏃‍♀️Course à pied")
-    st.write("Pas de parcours tracé mais un beau terrain de jeu trail à disposition.")
+    st.write("Pas de parcours tracé mais un beau terrain de jeu trail à disposition ⛰️")
+
+    st.subheader(" 📄Administratif")
+    st.write("🪪 Carte Européenne d’Assurance Maladie (CEAM). Elle n’est pas obligatoire, mais ceux qui étaient présents l’an dernier savent que ça peut être très utile en cas de besoin 🏥🇪🇺. 👉 Pensez à en faire la demande !")
+    lien_carte_eu = "https://www.ameli.fr/charente-maritime/assure/adresses-et-contacts/votre-carte-vitale-appli-carte-vitale-carte-europeenne-d-assurance-maladie-ceam/commander-une-carte-europeenne-d-assurance-maladie-ceam"
+    st.markdown(f"[Tu peux la demander sur ce lien]({lien_carte_eu})", unsafe_allow_html=True)
+
 # PARCOURS ############################# 
 
 elif menu == "Parcours":
@@ -103,6 +112,11 @@ elif menu == "Parcours":
             st.write(f"5️⃣ long de {df_choix["Kilometres"].iloc[4]}km pour {df_choix["Denivele"].iloc[4]}mD+")
         if nb_parcours >= 6 :
             st.write(f"6️⃣ long de {df_choix["Kilometres"].iloc[5]}km pour {df_choix["Denivele"].iloc[5]}mD+")
+        if nb_parcours >= 7 :
+            st.write(f"7️⃣ long de {df_choix["Kilometres"].iloc[6]}km pour {df_choix["Denivele"].iloc[6]}mD+")
+        if nb_parcours >= 8 :
+            st.write(f"8️⃣ long de {df_choix["Kilometres"].iloc[7]}km pour {df_choix["Denivele"].iloc[7]}mD+")
+
         
         choix = st.selectbox("Choisis le parcours que tu veux visualiser", list_nb_parcours)
 
@@ -115,5 +129,5 @@ elif menu == "Parcours":
 
 
     else :
-        st.write("pas de parcours")
+        st.write("❌ Pas de parcours avec ce kilométrage ❌")
     
