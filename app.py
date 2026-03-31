0b40e0227fd299aa40b1000a587d710f1055555b
Toine17 <Bersegeayantoine@yahoo.fr>
2026-03-31 14:08:54 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -57,44 +57,54 @@ elif menu == "Programme":
     st.subheader("Déroulé du stage")
     jour = st.selectbox("Choix de la journée" , ["Jour 1","Jour 2","Jour 3","Jour 4"])
     if jour == "Jour 1":
-        st.write("🚙 Départ ⏱️8h30 de la piscine de Surgères avec 2 minibus")
+        st.write("🚙 Départ ⏱️ horaire à définir de la piscine de Surgères avec 2 minibus")
         st.write("Trajet jusqu'en début d'après-midi, prévoir un pique-nique 🥪")
-        st.write("Arrivée au gîte, 🚴‍♂️ première sortie vélo de 48 km avec 1000 m D+")
+        st.write("Arrivée au gîte, 🚴‍♂️ première sortie vélo de 37km avec 750m D+")
         st.write("👕 Prévoir la tenue du club pour la photo de groupe du stage")
         st.write("Parcours")
         st.image("map_jour_1.jpg")
         st.write("Profil")
         st.image("Profil_jour_1.jpg")
-        dropbox_link_1 = "https://www.dropbox.com/scl/fi/4f85nhlffifvurx1i3ntn/Jour-1.gpx?rlkey=ds6h3gw7yy8ex0g7jwnu1j3aa&st=nhe3wcqc&dl=1"
+        dropbox_link_1 = "https://www.dropbox.com/scl/fi/m1r3h1jformh1yzn0sk34/GPX_J1.gpx?rlkey=bmksnn1sld2nnlukyblt7zeu7&st=jpryw3kf&dl=1"
         st.markdown(f"[📥 Télécharger le GPX]({dropbox_link_1})", unsafe_allow_html=True)
     if jour == "Jour 2":
-        st.write("🚴‍♂️ Sortie longue à vélo, 2 groupes pour 2 distances avec départ en commun")
-        st.write("Les 45 premiers kilomètres sont communs aux 2 groupes")
+        st.write("🚴‍♂️ Sortie longue à vélo, 3 groupes pour 3 distances")
+        st.write("Les débuts de parcours sont les mêmes donc les groupes peuvent partir ensembles ou il est possible de changer d'avis en cours de route")
         col1, col2 = st.columns(2)
 
         with col1:
             st.header("Version courte")
-            st.write("65km pour 1550m D+")
+            st.write("48km pour 1360m D+")
             st.write("Parcours")
             st.image("map_jour_2_courte.jpg")
             st.write("Profil")
             st.image("Profil_jour_2_courte.jpg")
-            dropbox_link_2_courte = "https://www.dropbox.com/scl/fi/7u6xlbv5ffxwhv4ofuryq/Jour-2-courte.gpx?rlkey=xk0k76z7o1dojs7ph2252h0qm&st=kvti2r3m&dl=1"
+            dropbox_link_2_courte = "https://www.dropbox.com/scl/fi/7v3cmzuzm92trgucuhqe3/GPX_J2-Courte.gpx?rlkey=d4l6czzlok1nf2t7b4iqydvi5&st=s7d06qno&dl=1"
             st.markdown(f"[📥 Télécharger le GPX]({dropbox_link_2_courte})", unsafe_allow_html=True)
+
+            st.header("Version moyenne")
+            st.write("66km pour 1770m D+")
+            st.write("Parcours")
+            st.image("map_jour_2_moyenne.jpg")
+            st.write("Profil")
+            st.image("Profil_jour_2_moyenne.jpg")
+            dropbox_link_2_moyenne = "https://www.dropbox.com/scl/fi/uvcyu3e7cc8onkgf66vs0/GPX_J2-Moyenne.gpx?rlkey=9m1m8jtpjp0bfxe5uj83w0pnz&st=xwv9nljc&dl=1"
+            st.markdown(f"[📥 Télécharger le GPX]({dropbox_link_2_moyenne})", unsafe_allow_html=True)
 
         with col2:
             st.header("Version longue")
-            st.write("92km pour 2150m D+")
+            st.write("98km pour 2990m D+")
             st.write("Parcours")
             st.image("map_jour_2_longue.jpg")
             st.write("Profil")
             st.image("Profil_jour_2_longue.jpg")
-            dropbox_link_2_longue = "https://www.dropbox.com/scl/fi/tarqflt0pd8nxyfh3qlqa/jour-2-longue.gpx?rlkey=dkod75gqul3nj54bypixpjvhy&st=ks2b427d&dl=1"
+            dropbox_link_2_longue = "https://www.dropbox.com/scl/fi/0ab6t59roz4p3yctvq0lr/GPX_J2-Longue.gpx?rlkey=iwjbq2zh1co9ud9xtlcl99mjz&st=3x9k5nqs&dl=1"
             st.markdown(f"[📥 Télécharger le GPX]({dropbox_link_2_longue})", unsafe_allow_html=True)
+
 
     if jour == "Jour 3":
             st.write("En autonomie, plusieurs parcours sont disponibles dans la section Parcours 🛣️")
-            st.write("Natation en mer en fin de journée à la plage de Zarautz🏊🏖️")
+            #st.write("Natation en mer en fin de journée à la plage de Zarautz🏊🏖️")
 
     if jour == "Jour 4":
             st.write("Courte activité le matin en autonomie")
@@ -104,25 +114,18 @@ elif menu == "Programme":
 
 elif menu == "Infos pratiques":
     st.subheader("🏨 Logement")
-    st.write("CARTE D'IDENTITE OBLIGATOIRE POUR S'ENREGISTRER")
-    st.write("Il se peut que nous ne soyons pas seuls dans le logement")
-    lien_logement = "https://www.gites.fr/gites_saskarate_asteasu_h5496447.htm"
-    st.write("4 chambres triples, 1 chambre quadruple 🛏️")
-    st.write("Selon l'agencement des chambres, nous essayerons de déplacer un lit pour créer une chambre double pour les filles et une deuxième chambre quadruple")
+    st.write("Nous serons seul dans le gîte, le nombre de chambres mises à disposition dépend du nombre de participants")
+    lien_logement = "https://www.lustou.com/camping-gite-groupe-vielle-aure/"
+    st.write("3 chambres de 5 🛏️")
     st.write("Les draps sont fournis, pensez à prendre votre carte d'identité")
     st.markdown(f"[Ici les informations sur le logement]({lien_logement})", unsafe_allow_html=True)
     
-    st.subheader("🏊 Natation")
-    st.write("La plage de Zarautz est à 30 minutes en voiture et la piscine municipale d'Aya à 17 minutes.")
-    st.write(f"Aujourd'hui l'eau de l'océan à la plage de Zarautz est à {st.session_state.temp_eau}°🌡️")
+    # st.subheader("🏊 Natation")
+    # st.write("La plage de Zarautz est à 30 minutes en voiture et la piscine municipale d'Aya à 17 minutes.")
+    # st.write(f"Aujourd'hui l'eau de l'océan à la plage de Zarautz est à {st.session_state.temp_eau}°🌡️")
 
     st.subheader(" 🏃‍♀️Course à pied")
     st.write("Pas de parcours tracé mais un beau terrain de jeu trail à disposition ⛰️")
-
-    st.subheader(" 📄Administratif")
-    st.write("🪪 Carte Européenne d’Assurance Maladie (CEAM). Elle n’est pas obligatoire, mais ceux qui étaient présents l’an dernier savent que ça peut être très utile en cas de besoin 🏥🇪🇺. 👉 Pensez à en faire la demande !")
-    lien_carte_eu = "https://www.ameli.fr/charente-maritime/assure/adresses-et-contacts/votre-carte-vitale-appli-carte-vitale-carte-europeenne-d-assurance-maladie-ceam/commander-une-carte-europeenne-d-assurance-maladie-ceam"
-    st.markdown(f"[Tu peux la demander sur ce lien]({lien_carte_eu})", unsafe_allow_html=True)
 
 # PARCOURS ############################# 
 
